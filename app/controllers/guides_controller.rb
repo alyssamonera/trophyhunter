cc168d74dfd3a15345fddcecc3fcc972c1b25235
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-09-06 10:45:01 -0500

    controller n model updated

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,8 +1,20 @@
 class GuidesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    render json: Guide.all
+  end
+
+  def show
+    render json: Guide.find(params["id"])
+  end
+
   def create
     render json: Guide.create(params["guide"])
+  end
+
+  def delete
+    render json: Guide.delete(params["id"])
   end
 
   def update
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -6,6 +6,36 @@ class Guide
     DB = PG.connect(host: "localhost", port: 5432, dbname: "trophyhunter_development")
   end
 
+  def self.all
+    results = DB.exec("SELECT * FROM guides;")
+    return results.map do |result|
+      {
+        id: result["id"].to_i,
+        title: result["title"],
+        username: result["username"],
+        body: result["body"],
+        url: result["url"]
+      }
+    end
+  end
+
+  def self.find(id)
+    results = DB.exec("SELECT * FROM guides WHERE id=#{id};")
+    if !results.num_tuples.zero?
+      return {
+        id: results.first["id"].to_i,
+        title: results.first["title"],
+        username: results.first["username"],
+        body: results.first["body"],
+        url: results.first["url"]
+      }
+    else
+      return {
+        "error" => "no such post, check the id!"
+      }, status: 400
+    end
+  end
+
   def self.create opts
     results = DB.exec(
       <<-SQL
@@ -22,6 +52,11 @@ class Guide
       body: result["body"],
       url: result["url"]
     }
+  end
+
+  def self.delete(id)
+    results = DB.exec("DELETE FROM guides WHERE id=#{id};")
+    return { "deleted" => true }
   end
 
   def self.update (id, opts)
