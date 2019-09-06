cd41377216c06e6e5fb7ab95545e9194110a04c2
jhwangit128 <peonyanko128@gmail.com>
2019-09-06 13:42:38 -0500

    main.js updated

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,22 @@
+// +++++++++++++++++++
+// DEPENDENCIES
+// +++++++++++++++++++
 import React from 'react'
+import Header from './Header.js'
+import Aside from './Aside.js'
+import Main from './Main.js'
 
+// +++++++++++++++++++
+// COMPONENT CLASS
+// +++++++++++++++++++
 class App extends React.Component {
+
+
+
+
+  // +++++++++++++++++++
+  // RENDER
+  // +++++++++++++++++++
   render () {
     return (
       <h1>testing!</h1>
@@ -8,4 +24,12 @@ class App extends React.Component {
   }
 }
 
+
+
+
+
+
+// +++++++++++++++++++
+// EXPORT
+// +++++++++++++++++++
 export default App
