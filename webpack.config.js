ecbb79180a0d92cbbd7bb84c29cac050ad521440
alyssadm <ahlisa.dm@gmail.com>
2019-09-07 12:23:43 -0400

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [ new webpack.optimize.UglifyJsPlugin({sourceMap: true}), ]
+};
