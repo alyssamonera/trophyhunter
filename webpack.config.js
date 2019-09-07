9b81d172b24f4ca614c6a79710a478f51e01779e
alyssadm <ahlisa.dm@gmail.com>
2019-09-07 13:43:42 -0400

    following react-quill instructions on fixing webpack error messages

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  module: {
+    // Shut off warnings about using pre-built javascript files
+    // as Quill.js unfortunately ships one as its `main`.
+    noParse: /node_modules\/quill\/dist/
+  }
+}
