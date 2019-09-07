eb3c81898069feb8948baf1ad8e97298b4842d33
alyssadm <ahlisa.dm@gmail.com>
2019-09-07 13:32:12 -0400

    removed seed stuff and removed webpacker custom config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Guide.create({
-  title: "[DAI] Astrarium puzzles",
-  username: "trinity",
-  body: <<-BODYSTART
-  Throughout five areas of Dragon Age Inquisition, you'll stumble across constellation challenges known as Astrarium puzzles. The trick is to connect up the dots in a certain order, so that you create the correct image. Think of constellations in the real world like The Plough or Orion's Belt for an idea of what's going on here.
-
-  While these challenges start off easily enough, they get increasingly trickier as you work through the whole series. It's worth the effort though, as once you've solved each set of three, you'll be guided towards a cave containing lots of delicious equipment. We've put together a quick guide - with fancy diagrams and everything - so you can tick them off in short order, and hoover up that lovely loot.
-
-  If you're looking for a more general guide to Dragon Age Inquisition, we've already put together a walkthrough for the core story, along with plenty of hints and tips for getting the most out of your Rogues, Warriors and Mages. Check out the links towards the bottom of this page for more details.
-        BODYSTART
-  ,
-  url: "https://www.eurogamer.net/articles/2014-11-28-dragon-age-inquisition-astrarium-puzzle-solutions"
-  })
