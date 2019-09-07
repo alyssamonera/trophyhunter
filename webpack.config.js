b145c5156087e915d8edf00824d14d85c2f27a19
alyssadm <ahlisa.dm@gmail.com>
2019-09-07 15:30:47 -0400

    removed all other instances of quill

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  module: {
-    // Shut off warnings about using pre-built javascript files
-    // as Quill.js unfortunately ships one as its `main`.
-    noParse: /node_modules\/quill\/dist/
-  }
-}
