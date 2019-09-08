779c814429fb182184d60c58abf36612b406e71a
jhwangit128 <peonyanko128@gmail.com>
2019-09-08 11:56:56 -0500

    seeds.rb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,13 +41,49 @@ Guides.create({
   You join Jesse Faden as she enters the mysteriously sinister Federal Bureau of Control in search for her younger brother, Dylan. The Bureau, however, is under attack from a force only known as the Hiss, whose motives are unknown.
   Jesse finds herself taking on the position of Director - the Head of Federal Bureau of Control. This makes her the only person who can stop the Hiss and she's not even getting paid. Control is divided up into a series of main missions.
   Each mission will take you to a new area in the Oldest House, the headquarters of the Federal Bureau of Control, as you try to stop the Hiss and find Dylan. Both the Hiss and a variety of puzzles, however, will try to stop you in your tracks.
-  The guides below will take you through each mission, explaining how to solve tricky puzzles and defeat the difficult Hiss.
-  We've also included the location of the various collectables you can discover in your travels through the Oldest House. These range from employee complaints to science presentations, with each one giving you an insight to life at the Bureau.
-  As you transverse the weird world of the Oldest House, you'll come across other oddities aside from your main mission to find Dylan.
-  This could be anything from tackling rogue Object of Power to helping out Ahti, the completely normal, janitor. Ahti thinks you're his new assistant and he doesn't want to miss his holiday, so you might as well do as he asks. Don't forget to upgrade Jesse! After completing a Mission you'll be given a number of Abilities points. You can use these to upgrade Jesse's health and various powers. You can do this at the Control Point in Central Executive.
   Always stay on the move in combat. Combat in Control is fast and, if you get stuck in a corner, you can find yourself dead very quickly. For this reason, it's a good idea to stay on the move, dodging bullets and throwing chairs at enemy Hiss.
   Deconstruct any unwanted mods. Deconstructing unwanted Personal and Weapon Mods will supply you with a small amount of Source Energy. You can then use this Source Energy to build new Weapon Forms for your Service Weapon to help you take out the Hiss.
   Collect as many collectables as you can. The Oldest House is full of collectables for you to find. Some are small easter eggs, others hint at dark events in the Bureau's past and a few will unlock new Side Missions for you to complete.",
   "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-08-23-09-26/Control_Pic_2.jpg/EG11/resize/690x-1/quality/75/format/jpg",
   "tags" => "supernatural, mystery"
+})
+Guides.create({
+  "title" => "FIFA 20 OTW cards",
+  "username" =>"tapsell",
+  "body" => "FIFA 20 Ones to Watch cards are a significant mechanic in Ultimate Team mode, collecting several hand-picked players under the age of 30 who secured high-profile transfers over the summer. A 'live' version of regular player cards in FUT, Ones to Watch add an intriguing option for players both on the pitch and in the ever-changing FUT Transfer Market, tracking players form over time. The key here will be picking the players who you feel will hit the greatest run of form this season. Wan-Bissaka, for instance, has had a very strong start for Manchester United, as has Joao Félix at Atletico Madrid. Keep a close eye on the values of the OTW cards that are the only ones for their position, like Wan-Bissaka so far - not because they're likely to have better seasons than anyone else, but because they could get a potentially dispriportionate boost to their value on the transfer market.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-09-05-10-37/fifa_20_ones_to_watch_1.jpg/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "fifa, sports"
+})
+Guides.create({
+  "title" => "Destiny 2",
+  "username" =>"redplanet",
+  "body" => "Destiny 2's Outbreak Perfected is an Exotic quest made available during Year 2.
+  It sees the return of original Destiny's Outbreak Prime as the renamed Outbreak Perfected, but thankfully this time the quest is a lot more straightforward to complete.
+  That said, it's not easy. Though the first half of the quest can be completed solo, the second and final half is an intense, lengthy mission that requires a dedicated Fireteam and lots of practice. Hope you're ready for a challenge! Scan the device within The Rig on Titan. Visit six Lost Sectors and find Fallen Transponder nodes. Visit The Farm and find the quest giver. Complete the Zero Hour mission within the time limit. With that done, Outbreak Perfected is yours. To start the Outbreak Perfected quest in Destiny 2, head to Titan's The Rig fast travel point, and head south across a bridge. This will take you to Tidal Anchor. From here, it's a fairly linear if winding stretch of corridors through the area, past a section with consoles and screens above them.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-08-21-16-16/destiny_2_outbreak_perfected_2.jpg",
+  "tags" => "battle, mission"
+})
+Guides.create({
+  "title" => "Red Dead Redemption 2",
+  "username" =>"toolguy",
+  "body" => "How to upgrade your camp and get Leather Working Tools: From Pearson, the camp cook, who can unlock a variety of cosmetic upgrades across the camp by donating items - mostly perfect pelts, hides and skins. He also deals with satchel upgrades. From the ledger. You won't get access to the ledger until you do your first debt collection as part of mission Money Lending and Other Sins. This is where the most beneficial upgrades come from, including fast travel. Once you've unlocked the ledger, it'll appear next to the camp donation box. Purchasing upgrades from the ledger will come out of camp funds first, and then from your own pocket. Unlocking the ledger is also where you get the Leather Working Tools. Head towards the back of the book, and there it is, for $225. Any upgrades you buy for the camp appear instantly - no need to wait around. The provisions they supply will be dotted around that location - for example, food to the left of where you check in with Pearson, or ammo round the back of Arthur's tent.
+  Remember, you need to stock up these provisions through the ledger if you use them up, and each upgrade will mean this restock will cost more.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2018/articles/2018-10-25-11-42/red_dead_redemption_2_camp_3.jpg/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "camp, map, tool"
+})
+Guides.create({
+  "title" => "Pokémon Go Spinda quest",
+  "username" =>"capgo04",
+  "body" => "Spinda is one of the most unique Pokémon in the entire series. Though the Normal-Type rabbit-like creature isn't particularly competent in battle, or evolves into anything unique, the pattern on its body is different with every catch - with over four billion possibilities touted in the main games.Each month, the Field Research you need to catch changes. For July, the quest is Five Great Curveball Throws in a Row with the Mystery Pokémon reward - with the reward being Spinda form 8. Once you have the task, then it's time to land a Curveball. Our Curveballs page can help if you're struggling with this throw type, but in short, you must spin the ball and land it in the circle. Take your time and wait until after an attack increase your chances.
+  If you're struggling, then attempting to catch a Legendary Pokémon from 7 Day Field Research - which usually have larger hit circles - might help.
+  After that, Spinda will appear when you claim the Field Research reward. Spinda, like other Field Research reward catches, won't run away, so take your time in catching it, and even consider using a Pinap Berry to increase the amount of Spinda Candy you earn.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2018/articles/2018-08-02-09-06/pokemon_go_spinda.jpg/EG11/resize/300x-1/quality/75/format/jpg",
+  "tags" => "monsters"
+})
+Guides.create({
+  "title" => "Gears 5 collectable locations",
+  "username" =>"tapsell",
+  "body" => "At the very start of the game, climb out of the water to your left, where there will be two paths - one going up, and another to the left by the cliff wall. At the end of this left path is the collectable on the ground. As you're exploring the ruined facility, you'll enter a bathroom, complete with showers and lockers. Halfway through on the left is a poster on the wall, which you can collect. This one comes a little while after the last two, after the battle through the second silo. After Dave lets you through a door and learns about some intelligence from a computer, you have to go left up some stairs to the Commander's office. Instead, go straight ahead to the Infirmary. On the right are several bodies in the ground - grab the collectable amongst the weapons and ammo. Straight after the above collectable, you'll climb some stairs on the way to the Commander's office objective. Ignore the corridor ahead and enter the dark room on your left. There's another room in here with the collectable on the desk.",
+  "image" => "https://guides.gamepressure.com/gfx/logos/640x384/980_427775651.jpg",
+  "tags" => "action, war"
 })
