8e33361386d3f913a215902ab61eca96eaad59bf
jhwangit128 <peonyanko128@gmail.com>
2019-09-08 11:32:01 -0500

    working in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,47 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Guides.create({
-  "title" => "Jake Peralta",
-  "username" =>"https://imgur.com/TDgdUdZ.png",
-  "body" => "The doctor said all my bleeding was internal -- that's where the blood's supposed to be"
+  "title" => "[DAI] Astrarium puzzles",
+  "username" =>"trinity",
+  "body" => "Throughout five areas of Dragon Age Inquisition, you'll stumble across constellation challenges known as Astrarium puzzles. The trick is to connect up the dots in a certain order, so that you create the correct image. Think of constellations in the real world like The Plough or Orion's Belt for an idea of what's going on here. While these challenges start off easily enough, they get increasingly trickier as you work through the whole series. It's worth the effort though, as once you've solved each set of three, you'll be guided towards a cave containing lots of delicious equipment. We've put together a quick guide - with fancy diagrams and everything - so you can tick them off in short order, and hoover up that lovely loot. If you're looking for a more general guide to Dragon Age Inquisition, we've already put together a walkthrough for the core story, along with plenty of hints and tips for getting the most out of your Rogues, Warriors and Mages.",
+  "image" => "https://i.ytimg.com/vi/miD4XD5oFGE/maxresdefault.jpg",
+  "tags" => "puzzle, dragon"
+})
+Guides.create({
+  "title" => "Monster Hunter World",
+  "username" =>"Reynolds",
+  "body" => "Ore are one of many classifications of components you'll collect as you play Monster Hunter World, and are used as a component for constructing weapons and armour pieces. Ores are primarily sourced from Mining Outcrops, which are a crystals in each of the game's hunting areas. Simply walk up to it, press the button prompt, and forage away. You get three ores per Mining Outcrop, and unlike previous Monster Hunter games, there's no need to have a pickaxe on you to mine them.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2018/articles/2018-02-02-11-14/mining.jpg/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "monsters, hunting"
+})
+Guides.create({
+  "title" => "Pokémon Go",
+  "username" =>"chacha90",
+  "body" => "Suicune is returning to raids in Pokémon Go for a limited time. The Legendary Pokémon will be available in raids between September 2nd and September 9th as part of the Global Challenge Ultra Bonus rewards. Though you might have thought it several times in the past - or received it as part of previously monthly Field Research - you might want a higher CP or IV version, or a chance to get that elusive shiny version - which is where learning the various Suicune counters and weaknesses comes in handy.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-09-02-18-19/pokemon_go_legendary_beasts.jpg/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "monsters"
+})
+Guides.create({
+  "title" => "Fortnite secret Battle Star locations",
+  "username" =>"jj1299",
+  "body" => "Each week in Fortnite, a new Loading Screen can be unlocked by completing certain challenges. If you view these loading screens closely, it actually hints at the location of a secret Battle Star or Banner location on the map. Secret Battle Stars and Loading Screens work a little differently in Fortnite Season X. Instead of being tied to completing all Challenges in a specific week, they now appear as part of time-limited Challenges - making them easier to unlock and locate. Here's examples of what they look like from time-limited challenges from the first two weeks (thanks ItzBullzye on reddit and itsZBG on reddit for these): Finding additional Battle Stars in the game will then give you an additional Tier towards your Season Battle Pass progress. You can also pick up new Banners, which will give you new customisation options. Like previous loading screen rewards, you need to have unlocked that specific loading screen before it will appear on the map. Since these only appear for a limited time, make sure you get it in your inventory before it disappears. But once you have it, you have until the end of the season to get it. Likely because of this, it appears there is no end-of-season bonus skin that will unlock for finding them all. Seasons 4 to 9 had these skins, named The Visitor, The Enforcer, AIM, The Prisoner and Ruin and Singularity respectively. As always, you have until the end of the Season to unlock the various Utopia loading screens and skin, otherwise it'll be gone forever. Good luck!",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-08-15-17-03/Fortnite_LocateBAttleStar_Zoomedin_Shop.png/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "battle, map"
+})
+Guides.create({
+  "title" => "Control",
+  "username" =>"lottie",
+  "body" => "Control is the latest supernatural thriller for PS4, Xbox One and PC.
+  You join Jesse Faden as she enters the mysteriously sinister Federal Bureau of Control in search for her younger brother, Dylan. The Bureau, however, is under attack from a force only known as the Hiss, whose motives are unknown.
+  Jesse finds herself taking on the position of Director - the Head of Federal Bureau of Control. This makes her the only person who can stop the Hiss and she's not even getting paid. Control is divided up into a series of main missions.
+  Each mission will take you to a new area in the Oldest House, the headquarters of the Federal Bureau of Control, as you try to stop the Hiss and find Dylan. Both the Hiss and a variety of puzzles, however, will try to stop you in your tracks.
+  The guides below will take you through each mission, explaining how to solve tricky puzzles and defeat the difficult Hiss.
+  We've also included the location of the various collectables you can discover in your travels through the Oldest House. These range from employee complaints to science presentations, with each one giving you an insight to life at the Bureau.
+  As you transverse the weird world of the Oldest House, you'll come across other oddities aside from your main mission to find Dylan.
+  This could be anything from tackling rogue Object of Power to helping out Ahti, the completely normal, janitor. Ahti thinks you're his new assistant and he doesn't want to miss his holiday, so you might as well do as he asks. Don't forget to upgrade Jesse! After completing a Mission you'll be given a number of Abilities points. You can use these to upgrade Jesse's health and various powers. You can do this at the Control Point in Central Executive.
+  Always stay on the move in combat. Combat in Control is fast and, if you get stuck in a corner, you can find yourself dead very quickly. For this reason, it's a good idea to stay on the move, dodging bullets and throwing chairs at enemy Hiss.
+  Deconstruct any unwanted mods. Deconstructing unwanted Personal and Weapon Mods will supply you with a small amount of Source Energy. You can then use this Source Energy to build new Weapon Forms for your Service Weapon to help you take out the Hiss.
+  Collect as many collectables as you can. The Oldest House is full of collectables for you to find. Some are small easter eggs, others hint at dark events in the Bureau's past and a few will unlock new Side Missions for you to complete.",
+  "image" => "https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-08-23-09-26/Control_Pic_2.jpg/EG11/resize/690x-1/quality/75/format/jpg",
+  "tags" => "supernatural, mystery"
 })
