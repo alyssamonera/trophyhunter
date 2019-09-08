7a6d5853663b37a4ebdcfd9540f9d3649ad21537
jhwangit128 <peonyanko128@gmail.com>
2019-09-08 10:29:45 -0500

    fixing footer

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -10,13 +10,11 @@ class Footer extends React.Component {
   render () {
     return (
       <footer className="footer" >
-          <div className="wrap">
-            <div className="copy">© 2019 Copyright</div>
             <div className="footer-names">
               <span>Alyssa Monera</span><br />
               <span>Jiha Hwang</span><br />
             </div>
-          </div>
+            <div className="copy">© 2019 Copyright</div>
       </footer>
     )
   }
